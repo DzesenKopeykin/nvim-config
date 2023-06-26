822511c9597515d2e929cd741dfbcd87df115ef6
DzesenKopeykin <kopeykindzzesen@gmail.com>
2023-06-26 20:34:56 +0300

    plug in packer

--- init.lua
+++ init.lua
@@ -1,13 +1,2 @@
-call plug#begin('~/.vim/plugged')
-
-Plug 'williamboman/mason.nvim'
-Plug 'williamboman/mason-lspconfig.nvim'
-Plug 'neovim/nvim-lspconfig'
-
-call plug#end()
-
-require("mason").setup()
-require("mason-lspconfig").setup({
-    ensure_installed = { "pylyzer" }
-})
+require("packer-config")
 
